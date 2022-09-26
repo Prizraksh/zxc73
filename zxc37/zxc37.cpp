c9b9afdb5e9fa1c085905b6cf33725ac4ec578f3
Prizraksh <Prizraksh@LAPTOP-1MRAK6NE>
2022-09-27 00:56:45 +0300

    Добавьте файлы проекта.

---
+++ zxc37/zxc37.cpp
@@ -0,0 +1,60 @@
+﻿/* 
+* Тема 1. 
+* 7. Оптовые закупки. Одна тетрадь стоит 20 центов.Тетради упакованы в пачки по 20 штук. 
+* Если покупать пачку тетрадей, то ее стоимость составит 3,7$. Пачки уложены в коробки по 20 пачек. При покупке 1 коробки взимается плата 70$. 
+* Составьте программу, определяющую по количеству тетрадей, 
+* которые хочет приобрести покупатель, число коробок, пачек и отдельных тетрадей, которые ему следует купить из условия максимальной выгоды 
+* (возможно, предлагающую приобрести большее количество тетрадей за те же или меньшие деньги с учетом оптовых цен), и подсчитывающую общую стоимость покупки.
+*/
+#include "iostream"
+using namespace std;
+
+int main()
+{
+	locale::global(locale(""));
+	int
+		amount_of_copybooks{}, // Общее количество тетрадей
+		count_of_packs{},	  // Количество пачек
+		count_of_boxes{};	  // Количество коробок
+	const int 
+		capacity_for_packs = 20, // Вместимость пачки
+		capacity_for_boxes = 400; // Вместимость коробки
+	const double
+		price_for_one_copybook = 0.2, // Цена за одну тетрадь
+		price_for_one_pack = 3.7,	  // Цена за одну пачку
+		price_for_one_box = 70;		  // Цена за одну коробку
+	double
+		total_price{}; // Общая цена
+	do
+	{
+		cout << "Введите количество тетрадей: ";
+		cin >> amount_of_copybooks;
+		if (amount_of_copybooks > 0)
+		{
+			break;
+		}
+	} while (true);
+	count_of_boxes = amount_of_copybooks / capacity_for_boxes; // Количество коробок, вмещаемых в себя тетради
+	amount_of_copybooks = amount_of_copybooks % capacity_for_boxes;
+    // amount_of_copybooks %= capacity_for_boxes; // строчки аналогичны
+	count_of_packs = amount_of_copybooks / capacity_for_packs; // Количество пачек, вмещаемых в себя тетради
+	amount_of_copybooks = amount_of_copybooks % capacity_for_packs;
+
+	total_price =  amount_of_copybooks * price_for_one_copybook +
+				   count_of_packs * price_for_one_pack +
+				   count_of_boxes * price_for_one_box;
+	cout << "Для максимальной выгоды нужно купить: " << endl;
+	if (amount_of_copybooks != 0)
+	{
+		cout << "Количество тетрадей: " << amount_of_copybooks << endl;
+	}
+	if (count_of_packs != 0)
+	{
+		cout << "Количество пачек: " << count_of_packs << endl;
+	}
+	if (count_of_boxes != 0)
+	{
+		cout << "Количество коробок: " << count_of_boxes << endl;
+	}
+	cout << "Общая сумма равна $" << total_price << endl;
+}
---
+++ zxc73/zxc73.cpp
@@ -0,0 +1,38 @@
+﻿#include "iostream"
+#include "locale.h"
+#include "stdio.h"
+#include "math.h"
+using namespace std;
+
+int t, p, k, n1, n2;
+double c;
+int main()
+{
+	locale::global(locale(""));
+	p = 0;
+	k = 0;
+	printf("\nВведите количество тетрадей: ");
+	scanf_s("%d", &t);
+	if (t >= 20)
+	{
+		int n1 = t / 20;
+		t = t - 20 * n1;
+		p = p + n1;
+	}
+	if (p >= 20)
+	{
+		int n2 = p / 20;
+		p = p - 20 * n2;
+		k = k + n2;
+	}
+	double c = (double)(t * 20 + p * 370 + k * 7000) / 100;
+	printf("\nДля макисмальной выгоды нужно купить: ");
+	printf("%d", t);
+	printf(", Пачек: ");
+	printf("%d", p);
+	printf(", Коробок: ");
+	printf("%d", k);
+	printf("\nСумма покупки ");
+	printf("%f", c);
+	printf(" долларов\n");
+}
